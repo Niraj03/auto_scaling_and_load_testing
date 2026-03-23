94c063c31a01107d5c78804527cd13e2094f88fc
nirajdharsandiya1313@gmail.com <nirajtv003.tracewave@gmail.com>
2026-03-23 13:23:36 +0530

    Fix: Removed serializer and send queryset directly.

--- users/views.py
+++ users/views.py
@@ -1,3 +1,4 @@
+from django.core.cache import cache
 from django.shortcuts import render
 from rest_framework import generics, status
 from rest_framework.response import Response
@@ -13,18 +14,33 @@ class ListOfUserAPIView(generics.ListAPIView):
     queryset = User.objects.all()
 
     def get(self, request, *args, **kwargs):
-        self.queryset = self.filter_queryset(self.get_queryset())
-        serializer = self.get_serializer(self.queryset, many=True)
-        data = serializer.data
-        new_data = {}
-        if not data:
-            new_data['count'] = 0
-            new_data['next'] = None
-            new_data['previous'] = None
-            new_data['results'] = data
-        else:
-            new_data['count'] = 0
-            new_data['next'] = None
-            new_data['previous'] = None
-            new_data['results'] = data
+        # redis_key = f"test_users_list"
+        # cached_data = cache.get(redis_key)
+        # if cached_data:
+        #     return Response({"data": cached_data, "message": USER_LIST}, status=status.HTTP_200_OK)
+        # self.queryset = self.filter_queryset(self.get_queryset())
+        # serializer = self.get_serializer(self.queryset, many=True)
+        # data = serializer.data
+        # new_data = {}
+        queryset = list(User.objects.all().values("id", "username", "email", "first_name", "last_name"))
+        users_list = list(queryset)
+
+        # ✅ 3. Build response
+        new_data = {
+            "count": len(users_list),
+            "next": None,
+            "previous": None,
+            "results": users_list
+        }
+        # if not data:
+        #     new_data['count'] = 0
+        #     new_data['next'] = None
+        #     new_data['previous'] = None
+        #     new_data['results'] = data
+        # else:
+        #     new_data['count'] = 0
+        #     new_data['next'] = None
+        #     new_data['previous'] = None
+        #     new_data['results'] = data
+        # cache.set(redis_key, new_data, timeout=60 * 10)
         return Response({"data": new_data, "message": USER_LIST}, status=status.HTTP_200_OK)
